e2af27f68f39e8b38f233f89162ba2e06a250c07
João Paulo Carvalho <jjpaulo2@protonmail.com>
2024-07-21 15:14:33 -0300

    feat - improvements in timer class

--- fastrpa/core/timer.py
+++ fastrpa/core/timer.py
@@ -12,19 +12,22 @@ class Timer:
     def __init__(self, webdriver: WebDriver):
         self.webdriver = webdriver
 
+    def wait(self, timeout_seconds: int = DEFAULT_TIMEOUT) -> WebDriverWait:
+        return WebDriverWait(self.webdriver, timeout_seconds)
+
     def wait_until_hide(
         self, xpath: str, timeout_seconds: int = DEFAULT_TIMEOUT
     ):
-        WebDriverWait(self.webdriver, timeout_seconds).until_not(
+        self.wait(timeout_seconds).until_not(
             expected_conditions.element_to_be_clickable((By.XPATH, xpath))
         )
 
     def wait_until_present(
         self, xpath: str, timeout_seconds: int = DEFAULT_TIMEOUT
     ):
-        WebDriverWait(self.webdriver, timeout_seconds).until(
+        self.wait(timeout_seconds).until(
             expected_conditions.element_to_be_clickable((By.XPATH, xpath))
         )
 
-    def wait_seconds(self, seconds: int):
+    def wait_seconds(self, seconds: float):
         sleep(seconds)
