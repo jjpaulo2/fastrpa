8558896eb468018a030c2e82fac3af445c75d6b0
João Paulo Carvalho <jjpaulo2@protonmail.com>
2024-07-21 20:16:45 -0300

    feat - take just screenshot bytes content

--- fastrpa/core/screenshot.py
+++ fastrpa/core/screenshot.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from selenium.webdriver.common.by import By
+from fastrpa.exceptions import ScreenshotNotTaken
 from fastrpa.types import WebDriver
 
 
@@ -11,6 +12,17 @@ class Screenshot:
         if path is None:
             path = f'{datetime.now().isoformat()}.png'
         return path
+    
+    def _restore_window(self, size: dict[str, int], position: dict[str, int], is_maximized: bool):
+        if not is_maximized:
+            self.webdriver.set_window_size(
+                size['width'], size['height']
+            )
+            self.webdriver.set_window_position(
+                position['x'], position['y']
+            )
+        else:
+            self.webdriver.maximize_window()
 
     @property
     def max_width(self) -> int:
@@ -23,24 +35,61 @@ class Screenshot:
         return self.webdriver.execute_script(
             'return document.documentElement.scrollHeight + (window.innerHeight * 0.2)'
         )
+    
+    @property
+    def is_maximized(self) -> bool:
+        window_size = self.webdriver.get_window_size()
+        screen_width = self.webdriver.execute_script("return screen.availWidth")
+        screen_height = self.webdriver.execute_script("return screen.availHeight")
+        return (window_size['width'] == screen_width) and (window_size['height'] == screen_height)
 
-    def viewport(self, path: str | None = None):
-        self.webdriver.save_screenshot(self._file_path(path))
-
-    def full_page(self, path: str | None = None):
+    @property
+    def image(self) -> bytes:
+        return self.webdriver.get_screenshot_as_png()
+    
+    @property
+    def full_page_image(self) -> bytes:
         starter_position = self.webdriver.get_window_position()
         starter_size = self.webdriver.get_window_size()
+        starter_is_maximized = self.is_maximized
+        image: bytes
 
         try:
             self.webdriver.set_window_size(self.max_width, self.max_height)
-            self.webdriver.find_element(By.XPATH, '//body').screenshot(
+            element = self.webdriver.find_element(By.XPATH, '//body')
+            image = element.screenshot_as_png
+
+        finally:
+            self._restore_window(
+                size=starter_size,
+                position=starter_position,
+                is_maximized=starter_is_maximized
+            )
+            return image
+
+    def save_image(self, path: str | None = None):
+        success = self.webdriver.save_screenshot(self._file_path(path))
+        if not success:
+            raise ScreenshotNotTaken('image', self.webdriver.current_url)
+        
+    def save_full_page_image(self, path: str | None = None):
+        starter_size = self.webdriver.get_window_size()
+        starter_position = self.webdriver.get_window_position()
+        starter_is_maximized = self.is_maximized
+
+        try:
+            self.webdriver.set_window_size(self.max_width, self.max_height)
+            element = self.webdriver.find_element(By.XPATH, '//body')
+            success = element.screenshot(
                 self._file_path(path)
             )
 
+            if not success:
+                raise ScreenshotNotTaken('image', self.webdriver.current_url)
+
         finally:
-            self.webdriver.set_window_size(
-                starter_size['width'], starter_size['height']
-            )
-            self.webdriver.set_window_position(
-                starter_position['x'], starter_position['y']
+            self._restore_window(
+                size=starter_size,
+                position=starter_position,
+                is_maximized=starter_is_maximized
             )
--- fastrpa/exceptions.py
+++ fastrpa/exceptions.py
@@ -1,33 +1,40 @@
 class ElementNotFound(Exception):
     message = 'No one element [{}] was found!'
 
-    def __init__(self, xpath: str) -> None:
+    def __init__(self, xpath: str):
         super().__init__(self.message.format(xpath))
 
 
 class ElementNotFoundAfterTime(Exception):
     message = 'Element [{}] not found after {} seconds!'
 
-    def __init__(self, xpath: str, timeout: int) -> None:
+    def __init__(self, xpath: str, timeout: int):
         super().__init__(self.message.format(xpath, timeout))
 
 
 class ElementNotCompatible(Exception):
     message = 'Element [{}] is not compatible with {}!'
 
-    def __init__(self, xpath: str, class_name: type) -> None:
+    def __init__(self, xpath: str, class_name: type):
         super().__init__(self.message.format(xpath, class_name))
+
+
+class ScreenshotNotTaken(Exception):
+    message = 'The [{}] screenshot from [{}] was not taken!'
+
+    def __init__(self, type: str, url: str):
+        super().__init__(self.message.format(type, url))
 
 
 class CookieNotAdded(Exception):
     message = 'The cookie [{}] was not added!'
 
-    def __init__(self, cookie_name: str) -> None:
+    def __init__(self, cookie_name: str):
         super().__init__(self.message.format(cookie_name))
 
 
 class KeyDoesNotExists(Exception):
     message = 'The key [{}] does not exists!'
 
-    def __init__(self, key: str) -> None:
+    def __init__(self, key: str):
         super().__init__(self.message.format(key))
