53903d6423a91905b34389b5e550298dd8b6c657
João Paulo Carvalho <jjpaulo2@protonmail.com>
2024-08-03 00:10:45 -0300

    style - aplicando ruff

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,6 @@ from selenium.webdriver import FirefoxOptions, ChromeOptions
 from unittest.mock import MagicMock
 
 
-
 def test_get_browser_options_default_options_class():
     options = utils.get_browser_options(['a', 'b'])
     assert type(options) is ChromeOptions
@@ -17,18 +16,16 @@ def test_get_browser_options():
 
 
 def test_get_session():
-    cookies_mock = [{
-        'name': 'test',
-        'value': 'test_value',
-        'domain': 'www.site.com',
-        'path': '/',
-        'secure': False
-    }]
-    webdriver_mock = MagicMock(
-        get_cookies=MagicMock(
-            return_value=cookies_mock
-        )
-    )
+    cookies_mock = [
+        {
+            'name': 'test',
+            'value': 'test_value',
+            'domain': 'www.site.com',
+            'path': '/',
+            'secure': False,
+        }
+    ]
+    webdriver_mock = MagicMock(get_cookies=MagicMock(return_value=cookies_mock))
     session = utils.get_session(webdriver_mock)
     assert len(session.cookies) == 1
     assert session.cookies.get('test') == 'test_value'
@@ -37,4 +34,3 @@ def test_get_session():
 def test_get_domain():
     webdriver_mock = MagicMock(current_url='https://www.site.com/')
     assert utils.get_domain(webdriver_mock) == 'www.site.com'
-
